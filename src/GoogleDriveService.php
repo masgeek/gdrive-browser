326594eb7bbc6f59e51289435827c86dd414ec7b
Masgeek <barsamms@gmail.com>
2025-02-26 09:50:18 +0300

    refactor(GoogleDriveService): extend from BaseGoogleService to streamline initialization 💡

--- src/GoogleDriveService.php
+++ src/GoogleDriveService.php
@@ -18,7 +18,7 @@ use Psr\Cache\InvalidArgumentException;
 /* The `GoogleDriveService` class is a PHP class that provides functionality to interact with Google
 Drive API */
 
-class GoogleDriveService {
+class GoogleDriveService extends BaseGoogleService {
 	private Google_Client $client;
 	private Google_Service_Drive $service;
 
@@ -85,42 +85,6 @@ class GoogleDriveService {
 
 		$this->service         = new Google_Service_Drive( $this->client );
 		$this->currentFolderId = $current_folder_id;
-		$this->cacheService    = new CacheService();
-	}
-
-	public function __construct_old( string $folderId = null ) {
-		$dotenv = Dotenv::createImmutable( dirname( __DIR__ ) );
-
-		$dotenv->load();
-
-		$dotenv->required( [
-			'GOOGLE_CREDENTIALS_PATH',
-			'GOOGLE_SERVICE_ACCOUNT',
-			'GOOGLE_DEFAULT_FOLDER_ID',
-			'GOOGLE_APPLICATION_NAME'
-		] )->notEmpty();
-
-		$credentialsPath = $_ENV['GOOGLE_CREDENTIALS_PATH'];
-		$appName         = $_ENV['GOOGLE_APPLICATION_NAME'];
-		$serviceAccount  = $_ENV['GOOGLE_SERVICE_ACCOUNT'];
-		if ( $folderId === null ) {
-			$defaultFolderId = $_ENV['GOOGLE_DEFAULT_FOLDER_ID'];
-		} else {
-			$defaultFolderId = $folderId;
-		}
-
-
-		$this->client = new Google_Client();
-		$this->client->setApplicationName( $appName );
-		$this->client->setScopes( Google_Service_Drive::DRIVE_READONLY );
-		$this->client->setAuthConfig( $credentialsPath );
-
-		if ( $serviceAccount ) {
-			$this->client->setSubject( $serviceAccount );
-		}
-
-		$this->service         = new Google_Service_Drive( $this->client );
-		$this->currentFolderId = $defaultFolderId;
 		$this->cacheService    = new CacheService();
 	}
 
